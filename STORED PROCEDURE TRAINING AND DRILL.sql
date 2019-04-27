c5d03b72a82e9f4c922d32a256a64f46db568b86
Nicson Martinez <martineznicson@gmail.com>
2019-04-26 20:25:52 -0700

    Upate file

--- STORED PROCEDURE TRAINING AND DRILL.sql
+++ STORED PROCEDURE TRAINING AND DRILL.sql
@@ -740,10 +740,15 @@ DROP PROCEDURE dbo.TestToDELETE1, dbo.TestToDELETE2, dbo.TestToDELETE3
 Modifying an existing SQL Server stored procedure
 
 Overview
-When you first create your stored procedures it may work as planned, but how to do you modify an existing stored procedure.  In this topic we look at the ALTER PROCEDURE command and it is used.
+When you first create your stored procedures it may work as planned, but how to do you modify an existing stored procedure.  
+In this topic we look at the ALTER PROCEDURE command and it is used.
 
 Explanation
-Modifying or ALTERing a stored procedure is pretty simple.  Once a stored procedure has been created it is stored within one of the system tables in the database that is was created in.  When you modify a stored procedure the entry that was originally made in the system table is replaced by this new code.  Also, SQL Server will recompile the stored procedure the next time it is run, so your users are using the new logic.  The command to modify an existing stored procedure is ALTER PROCEDURE or ALTER PROC.
+Modifying or ALTERing a stored procedure is pretty simple.  Once a stored procedure has been created it is stored within 
+one of the system tables in the database that is was created in.  When you modify a stored procedure the entry that was 
+originally made in the system table is replaced by this new code.  Also, SQL Server will recompile the stored procedure 
+the next time it is run, so your users are using the new logic.  The command to modify an existing stored procedure is 
+ALTER PROCEDURE or ALTER PROC.
 
 Modifying an Existing Stored Procedure
 Let's say we have the following existing stored procedure:  This allows us to do an exact match on the City.
